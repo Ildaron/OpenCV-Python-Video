b787027ad1f2f405bd899c1ee5577d569f684e83
Ildar <de.ikar@yandex.ru>
2021-03-28 21:34:04 +0900

    Update 10.2Cameras_in_one_window.py

--- 10.2Cameras_in_one_window.py
+++ 10.2Cameras_in_one_window.py
@@ -2,11 +2,44 @@ import cv2
 import time
 import numpy as np
 cap = cv2.VideoCapture(0)
+x1=000
+y1=100
+x2=900
+y2=100
 
 while(cap.isOpened()):
-    ret, current_frame = cap.read()
+    ret, image1 = cap.read()
+    ret, image2 = cap.read()
+    #current_frame1 = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY)
 
-    both = np.concatenate((current_frame, current_frame), axis=1)  
+    line_thickness = 2
+    cv2.line(image1, (x1, y1+50), (x2, y2+50), (10, 155, 10), thickness=line_thickness)
+    cv2.line(image1, (x1, y1-50), (x2, y2-50), (90, 55, 60), thickness=line_thickness)
+    cv2.line(image1, (x1, y1+150), (x2, y2+150), (90, 55, 60), thickness=line_thickness)
+    cv2.line(image1, (x1, y1), (x2, y2), (60, 70, 100), thickness=line_thickness)
+
+    cv2.line(image1, (x1, y1+450), (x2, y2+450), (10, 155, 10), thickness=line_thickness)
+    cv2.line(image1, (x1, y1+300), (x2, y2+300), (5, 15, 60), thickness=line_thickness)
+    cv2.line(image1, (x1, y1+100), (x2, y2+100), (67, 45, 60), thickness=line_thickness)
+    cv2.line(image1, (x1, y1+200), (x2, y2+200), (167, 145, 160), thickness=line_thickness)
+    cv2.line(image1, (x1, y1+250), (x2, y2+250), (160, 170, 10), thickness=line_thickness)
+
+
+
+    cv2.line(image2, (x1, y1+50), (x2, y2+50), (10, 155, 10), thickness=line_thickness)
+    cv2.line(image2, (x1, y1-50), (x2, y2-50), (90, 55, 60), thickness=line_thickness)
+    cv2.line(image2, (x1, y1+150), (x2, y2+150), (90, 55, 60), thickness=line_thickness)
+    cv2.line(image2, (x1, y1), (x2, y2), (60, 70, 100), thickness=line_thickness)
+
+    cv2.line(image2, (x1, y1+450), (x2, y2+450), (10, 155, 10), thickness=line_thickness)
+    cv2.line(image2, (x1, y1+300), (x2, y2+300), (5, 15, 60), thickness=line_thickness)
+    cv2.line(image2, (x1, y1+100), (x2, y2+100), (67, 45, 60), thickness=line_thickness)
+    cv2.line(image2, (x1, y1+200), (x2, y2+200), (167, 145, 160), thickness=line_thickness)
+    cv2.line(image2, (x1, y1+250), (x2, y2+250), (160, 170, 10), thickness=line_thickness)
+
+
+    
+    both = np.concatenate((image1, image2), axis=1)
     cv2.imshow('frame diff ',both)      
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
